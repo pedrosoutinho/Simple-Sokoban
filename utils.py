fa65f5b998fa64708b4b41aa29c754c2e5810949
pedrosoutinho <johnetterna@protonmail.com>
2024-04-28 22:06:07 -0300

    added parseLevel to utils.py

--- utils.py
+++ utils.py
@@ -1,44 +1,56 @@
-import enum from Enum
+from enum import Enum
 
-class Cell(Enum):
+
+class Tile(Enum):
     EMPTY = 0
     BOX = 1
     TARGET = 2
     WALL = 3
     PLAYER = 4
+    TARGET_BOX = 5
 
     def __str__(self) -> str:
+        assert self in Tile, "Invalid tile"
         return {
-            Cell.EMPTY: ' ',
-            Cell.BOX: '$',
-            Cell.TARGET: '.',
-            Cell.WALL: '#',
-            Cell.PLAYER: '@'
+            Tile.EMPTY: ' ',
+            Tile.BOX: '$',
+            Tile.TARGET: '.',
+            Tile.WALL: '#',
+            Tile.PLAYER: '@',
+            Tile.TARGET_BOX: '*'
         }[self]
 
-def parseTile(tile: str) -> Cell:
+
+def parseTile(char: str) -> Tile:
+    assert len(char) == 1, "Invalid tile character"
     return {
-        ' ': Cell.EMPTY,
-        '$': Cell.BOX,
-        '.': Cell.TARGET,
-        '#': Cell.WALL,
-        '@': Cell.PLAYER
-    }[tile]
+        ' ': Tile.EMPTY,
+        '$': Tile.BOX,
+        '.': Tile.TARGET,
+        '#': Tile.WALL,
+        '@': Tile.PLAYER,
+        '*': Tile.TARGET_BOX
+    }[char]
+
+
+def parseLevel(levelData: str) -> list[list[Tile]]:
+    lines = levelData.splitlines()
+    return [[parseTile(char) for char in line] for line in lines]
+
 
 class Vector2D:
     x: int
     y: int
 
-    def __init__(self, x: int, y: int):
+    def __init__(self, x: int, y: int) -> None:
         self.x = x
         self.y = y
 
     def __add__(self, other: 'Vector2D') -> 'Vector2D':
         return Vector2D(self.x + other.x, self.y + other.y)
 
-    def value(self, state: List[List[Cell]]) -> Cell:
+    def value(self, state: list[list[Tile]]) -> Tile:
         return state[self.y][self.x]
 
-    def setValue(self, state: List[List[Cell]], value: Cell):
+    def setValue(self, state: list[list[Tile]], value: Tile) -> None:
         state[self.y][self.x] = value
-
